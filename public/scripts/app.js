90bf665be243b66bbb93c7d9e2b52a3f60cc2b74
Bill Bunkum <billbunkum@Billy-Boy-Uno.local>
2023-06-06 17:35:14 -0400

    toy with React

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,6 +3,6 @@
 console.log('app.js is running');
 
 // JSX - Javascript XML provided by ReactJS
-var template = /*#__PURE__*/React.createElement("p", null, "Does this Change????!");
+var template = /*#__PURE__*/React.createElement("p", null, "~Testing~");
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 console.log('app.js is running');
 
 // JSX - Javascript XML provided by ReactJS
-let template = <p>Does this Change????!</p>;
+let template = <p>
+  ~Testing~
+</p>;
+
 let appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
